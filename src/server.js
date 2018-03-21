f5e68d13d7b8f78dde9298238037f14f76bc38ba
YoProgrammer <yuttasakcom@gmail.com>
2018-03-21 23:31:44 +0700

    update client ip

--- src/server.js
+++ src/server.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.end(req.connection.remoteAddress)
+  const ip = req.header('x-forwarded-for') || req.connection.remoteAddress
+  res.end(ip)
 })
 
 app.listen(3000)
